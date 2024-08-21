16f7ff12a70ca6a1b8beaa648b90562298ee2cb3
Tophervaldez19 <65585484+Tophervaldez19@users.noreply.github.com>
2024-08-21 21:36:26 +0800

    Pizza Type Entity and configuration

---
+++ src/Domain/Entities/PizzaType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPlace.Domain.Entities;
+public class PizzaType : BaseAuditableEntity<string>
+{    
+    public string Name { get; set; } = string.Empty;
+    public string Category { get; set;} = string.Empty;
+    public string Ingredients { get; set;} = string.Empty;
+}
---
+++ src/Infrastructure/Data/Configurations/PizzaTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PizzaPlace.Domain.Entities;
+
+namespace PizzaPlace.Infrastructure.Data.Configurations;
+public class PizzaTypeConfiguration : IEntityTypeConfiguration<PizzaType>
+{
+    public void Configure(EntityTypeBuilder<PizzaType> builder)
+    {
+        builder.Property(x => x.Id)
+            .ValueGeneratedNever();
+    }
+}
