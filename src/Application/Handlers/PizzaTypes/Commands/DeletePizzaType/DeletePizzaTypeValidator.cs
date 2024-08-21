362df509954de7f07acd9833ebbe589e10877718
Tophervaldez19 <65585484+Tophervaldez19@users.noreply.github.com>
2024-08-21 23:13:06 +0800

    Delete PizzaType Endpoint

---
+++ src/Application/Handlers/PizzaTypes/Commands/DeletePizzaType/DeletePizzaTypeValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PizzaPlace.Application.Common.Interfaces;
+
+namespace PizzaPlace.Application.Handlers.PizzaTypes.Commands.DeletePizzaType;
+public class DeletePizzaTypeValidator :AbstractValidator<DeletePizzaTypeCommand>
+{
+    private readonly IApplicationDbContext _context;
+    public DeletePizzaTypeValidator(IApplicationDbContext context)
+    {
+        _context = context;
+
+        RuleFor(x => x.Id).NotEmpty()
+            .MustAsync(IdExist)
+            .WithMessage((command, id) => $"The Id:{id} does not exists.");
+    }
+
+    private async Task<bool> IdExist(string id, CancellationToken cancellationToken)
+    {
+        // checks if a pizza type with the given Id already exists
+        return await _context.PizzaTypes.AnyAsync(x => x.Id == id, cancellationToken);
+    }
+}
--- src/Web/Endpoints/PizzaTypes.cs
+++ src/Web/Endpoints/PizzaTypes.cs
@@ -1,4 +1,5 @@
-﻿using PizzaPlace.Application.Handlers.PizzaTypes.Commands;
+﻿using PizzaPlace.Application.Handlers.PizzaTypes.Commands.CreatePizzaType;
+using PizzaPlace.Application.Handlers.PizzaTypes.Commands.DeletePizzaType;
 
 namespace PizzaPlace.Web.Endpoints;
 
@@ -7,11 +8,24 @@ public class PizzaTypes :EndpointGroupBase
     public override void Map(WebApplication app)
     {
         app.MapGroup(this)
-            .MapPost(CreatePizzaType);
+            .MapPost(CreatePizzaType)
+            .MapPost(UploadPizzaType, nameof(UploadPizzaType))
+            .MapDelete(DeletePizzaType, "DeletePizzaType/{id}");
+            
     }
 
     public async Task<Result> CreatePizzaType(ISender sender,CreatePizzaTypeCommand command)
     {
         return await sender.Send(command);
+    }
+
+    public async Task<Result> DeletePizzaType(ISender sender, string id)
+    {
+        return await sender.Send(new DeletePizzaTypeCommand() { Id = id });
+    }
+
+    public Result UploadPizzaType(ISender sender,IFormFile file)
+    {
+        return Result.Success();
     }
 }
