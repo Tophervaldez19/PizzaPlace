cd6990039a8e16fe666a8f02438fb7d4aff2a76c
Tophervaldez19 <65585484+Tophervaldez19@users.noreply.github.com>
2024-08-21 22:37:54 +0800

    Create Pizza Type API Endpoint.

---
+++ src/Web/Endpoints/PizzaTypes.cs
@@ -0,0 +1,17 @@
+﻿using PizzaPlace.Application.Handlers.PizzaTypes.Commands;
+
+namespace PizzaPlace.Web.Endpoints;
+
+public class PizzaTypes :EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .MapPost(CreatePizzaType);
+    }
+
+    public async Task<Result> CreatePizzaType(ISender sender,CreatePizzaTypeCommand command)
+    {
+        return await sender.Send(command);
+    }
+}
