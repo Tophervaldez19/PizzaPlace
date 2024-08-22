3d3b810c719d8281a2b824cffd5c56ceb25b388d
Tophervaldez19 <65585484+Tophervaldez19@users.noreply.github.com>
2024-08-22 12:52:46 +0800

    DeleteOrder

---
+++ src/Web/Endpoints/Orders.cs
@@ -0,0 +1,25 @@
+﻿
+using PizzaPlace.Application.Handlers.Orders.Commands.CreateOrder;
+using PizzaPlace.Application.Handlers.Orders.Commands.DeleteOrder;
+
+namespace PizzaPlace.Web.Endpoints;
+
+public class Orders : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .MapPost(CreateOrder)
+            .MapDelete(DeleteOrder, "DeleteOrder/{id}");
+    }
+
+    public async Task<Result> CreateOrder(ISender sender, CreateOrderCommand command)
+    {
+        return await sender.Send(command);
+    }
+
+    public async Task<Result> DeleteOrder(ISender sender, int id)
+    {
+        return await sender.Send(new DeleteOrderCommand() { Id = id });
+    }
+}
