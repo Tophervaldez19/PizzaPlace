ed137366238a3e003847694f917fd0ce2f9c8929
Tophervaldez19 <65585484+Tophervaldez19@users.noreply.github.com>
2024-08-22 12:36:50 +0800

    Add Order Entity and migration

---
+++ src/Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PizzaPlace.Domain.Entities;
+
+namespace PizzaPlace.Infrastructure.Data.Configurations;
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.Property(x => x.Id)
+            .ValueGeneratedNever();
+    }
+}
