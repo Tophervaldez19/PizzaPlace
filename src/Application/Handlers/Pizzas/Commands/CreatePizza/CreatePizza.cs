4e7e33a6a14dd2807a54c792dcea9281bea6005c
Tophervaldez19 <65585484+Tophervaldez19@users.noreply.github.com>
2024-08-22 11:28:36 +0800

    Create Pizza

---
+++ src/Application/Handlers/Pizzas/Commands/CreatePizza/CreatePizza.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PizzaPlace.Application.Common.Interfaces;
+using PizzaPlace.Domain.Enums;
+
+namespace PizzaPlace.Application.Handlers.Pizzas.Commands.CreatePizza;
+public class CreatePizzaCommand : IRequest<Result>
+{
+    public string Id { get; set; } = String.Empty;
+    public string PizzaTypeId { get; set; } = String.Empty;
+    public PizzaSize Size { get; set; }
+    public double Price { get; set; }
+}
+
+public class CreatePizzaCommandHandler(IApplicationDbContext context,IMapper mapper) : IRequestHandler<CreatePizzaCommand, Result>
+{
+    private readonly IApplicationDbContext _context = context;
+    private readonly IMapper _mapper = mapper;
+    public async Task<Result> Handle(CreatePizzaCommand request, CancellationToken cancellationToken)
+    {
+        var pizza = _mapper.Map<Pizza>(request);
+
+        await _context.Pizzas.AddAsync(pizza);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
---
+++ src/Application/Handlers/Pizzas/Commands/CreatePizza/CreatePizzaValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PizzaPlace.Application.Common.Interfaces;
+
+namespace PizzaPlace.Application.Handlers.Pizzas.Commands.CreatePizza;
+public class CreatePizzaValidator : AbstractValidator<CreatePizzaCommand>
+{
+    private readonly IApplicationDbContext _context;
+    public CreatePizzaValidator(IApplicationDbContext context)
+    {
+        _context = context;
+
+        RuleFor(x => x.Id).NotEmpty()
+            .MustAsync(IdDoesNotExist)
+            .WithMessage((command, id) => $"The Id:{id} already exists.");
+
+        RuleFor(x => x.PizzaTypeId).NotEmpty()
+            .MustAsync(TypeIdExist)
+            .WithMessage((command) => $"The Pizza Type Id:{command.PizzaTypeId} does not exists.");
+
+        RuleFor(x => x.Size).NotEmpty();
+        RuleFor(x => x.Price).NotEmpty();
+    }
+
+    private async Task<bool> IdDoesNotExist(string id, CancellationToken cancellationToken)
+    {
+        // checks if a pizza type with the given Id already exists
+        return !await _context.Pizzas.AnyAsync(x => x.Id == id, cancellationToken);
+    }
+
+    private async Task<bool> TypeIdExist(string typeId, CancellationToken cancellationToken)
+    {
+        // checks if a pizza type with the given Id already exists
+        return await _context.PizzaTypes.AnyAsync(x => x.Id == typeId, cancellationToken);
+    }
+}
