17520e292c07e26378decf0e2a36794fa2fda486
Tophervaldez19 <65585484+Tophervaldez19@users.noreply.github.com>
2024-08-22 09:13:48 +0800

    PizzaTypeCsv Dto and Mapping

---
+++ src/Application/Common/Helpers/CsvMappings/PizzaTypeMapping.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+using PizzaPlace.Application.Common.Helpers.CsvDtos;
+
+namespace PizzaPlace.Application.Common.Helpers.CsvMappings;
+public class PizzaTypeMapping : ClassMap<PizzaTypeCsvDto>
+{
+    public PizzaTypeMapping()
+    {
+        Map(x => x.Id)
+            .Name("pizza_type_id")
+            .Validate(field =>
+            {
+                if (string.IsNullOrEmpty(field.Field))
+                    throw new Exception($"Id is required");
+                return true;
+            });
+
+        Map(x => x.Name)
+            .Name("name")
+            .Validate(field =>
+            {
+                if (string.IsNullOrEmpty(field.Field))
+                    throw new Exception($"Name is required");
+                return true;
+            });
+
+        Map(x => x.Category)
+            .Name("category")
+            .Validate(field =>
+            {
+                if (string.IsNullOrEmpty(field.Field))
+                    throw new Exception($"Category is required");
+                return true;
+            });
+
+        Map(x => x.Ingredients)
+            .Name("ingredients")
+            .Validate(field =>
+            {
+                if (string.IsNullOrEmpty(field.Field))
+                    throw new Exception($"Ingredients is required");
+                return true;
+            });
+    }
+}
